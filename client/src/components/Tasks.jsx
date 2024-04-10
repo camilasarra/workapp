219031c7c40d04d2ef6ea5d3a002ab461179ae15
camilasarra <camilasarra@gmail.com>
2024-04-10 13:36:06 -0300

    add-features

--- client/src/components/Tasks.jsx
+++ client/src/components/Tasks.jsx
@@ -1,20 +1,19 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
 
-
-const API_BASE = 'http://localhost:3001'; // Cambia la URL base a localhost:3001
+const API_BASE = 'http://localhost:3001';
 
 function Tasks() {
-  
   const [items, setItems] = useState([]);
+  const [input, setInput] = useState("");
 
   useEffect(() => {
-    GetTasks();
+    getTasks();
   }, []);
 
-  const GetTasks = async () => {
+  const getTasks = async () => {
     try {
-      const response = await axios.get(`${API_BASE}/tasks`); // Cambia la ruta a /tasks
+      const response = await axios.get(`${API_BASE}/tasks`);
       setItems(response.data);
     } catch (error) {
       console.error(error);
@@ -24,11 +23,24 @@ function Tasks() {
   const handleDeleteTask = async (taskId) => {
     try {
       await axios.delete(`${API_BASE}/task/${taskId}`);
-      // Vuelve a cargar las tareas después de la eliminación
-      GetTasks();
+      getTasks();
     } catch (error) {
       console.error('Error deleting task:', error);
     }
+  }
+
+  const handleAddTask = async () => {
+    try {
+      const response = await axios.post(`${API_BASE}/tasks`, { name: input });
+      setInput("");
+      getTasks(); 
+    } catch (error) {
+      console.error('Error adding task:', error);
+    }
+  }
+
+  const handleChange = (e) => {
+    setInput(e.target.value);
   }
 
   return (
@@ -38,19 +50,19 @@ function Tasks() {
       </div>
 
       <div className="form">
-        <input type='text'></input>
-        <button>
+        <input type='text' value={input} onChange={handleChange}></input>
+        <button onClick={handleAddTask}>
           <span>ADD</span>
         </button>
       </div>
 
       <div className="todolist">  
-      
-        {items.map((item, index) => (
-          <div className="todo" key={index}>
-            <div className="text">{item.text}</div> 
+        {items.map((item) => (
+          <div className="todo" key={item._id}>
+            <div className="text">{item.name}</div>
             <div className="delete-todo" 
-            onClick={() => handleDeleteTask(item.id)}><span>X</span></div>
+            onClick={() => handleDeleteTask(item._id)}><span>X</span>
+            </div>
           </div>
         ))}
       </div>
